35889a525b4bcb3d830bf1ba43c947e724e39828
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-09-02 21:32:18 +0200

    first commit

---
+++ scripts/provision-vagrant-user.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Install pyenv"
+touch ~/.bash_profile
+grep -q -F "export PATH=\"/home/vagrant/.pyenv/bin:\$PATH\"" ~/.bash_profile || echo "export PATH=\"/home/vagrant/.pyenv/bin:\$PATH\"" >> ~/.bash_profile
+source ~/.bash_profile
+grep -q -F "eval \"\$(pyenv init -)\"" ~/.bash_profile || echo "eval \"\$(pyenv init -)\"" >> ~/.bash_profile
+grep -q -F "eval \"\$(pyenv virtualenv-init -)\"" ~/.bash_profile || echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bash_profile
+curl -s -S -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+source ~/.bash_profile
+
+echo "Install Python (Anaconda with Python 3)"
+pyenv install anaconda3-2020.02
+
+echo "Activate Python"
+cd /home/vagrant/data
+pyenv local anaconda3-2020.02
+pip install histolab --user
+
+echo "Setup login directory"
+echo "cd /home/vagrant/data/"  >> /home/vagrant/.bash_profile
+echo "source ~/.bashrc"  >> /home/vagrant/.bash_profile
